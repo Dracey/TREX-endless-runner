f37e9544c3c1f48c34c51f2c4e4261ea348a05e2
Maria Maas <meagmaas@student.che.nl>
2023-05-08 14:13:49 +0200

    First project to learn C# and Microsoft Form with TREX endless runner.  Made with this tutorial: https://www.youtube.com/watch?v=35RTKAFDjY0

--- TREX endless runner/Form1.Designer.cs
+++ TREX endless runner/Form1.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            trex = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            textScore = new Label();
+            gameTimer = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)trex).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // trex
+            // 
+            trex.Image = Properties.Resources.running;
+            trex.Location = new Point(89, 361);
+            trex.Name = "trex";
+            trex.Size = new Size(40, 43);
+            trex.SizeMode = PictureBoxSizeMode.AutoSize;
+            trex.TabIndex = 1;
+            trex.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.obstacle_1;
+            pictureBox3.Location = new Point(370, 358);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(23, 46);
+            pictureBox3.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            pictureBox3.Tag = "obstacle";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.obstacle_2;
+            pictureBox4.Location = new Point(564, 371);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(32, 33);
+            pictureBox4.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            pictureBox4.Tag = "obstacle";
+            // 
+            // textScore
+            // 
+            textScore.AutoSize = true;
+            textScore.Font = new Font("Consolas", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            textScore.Location = new Point(12, 9);
+            textScore.Name = "textScore";
+            textScore.Size = new Size(106, 24);
+            textScore.TabIndex = 4;
+            textScore.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            gameTimer.Interval = 20;
+            gameTimer.Tick += MainGameTimerEvent;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Black;
+            pictureBox1.Location = new Point(-6, 404);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(913, 50);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonHighlight;
+            ClientSize = new Size(882, 447);
+            Controls.Add(pictureBox1);
+            Controls.Add(textScore);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(trex);
+            Name = "Form1";
+            Tag = "obstacle";
+            Text = "TREX endless runner";
+            KeyDown += keyisdown;
+            KeyUp += keyisup;
+            ((System.ComponentModel.ISupportInitialize)trex).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private PictureBox trex;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private Label textScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private PictureBox pictureBox1;
     }
 }
--- TREX endless runner/Form1.cs
+++ TREX endless runner/Form1.cs
@@ -1,10 +1,130 @@
+using System.DirectoryServices.ActiveDirectory;
+
 namespace TREX_endless_runner
 {
     public partial class Form1 : Form
     {
+        bool jumping = false;
+        int jumpSpeed;
+        int force = 12;
+        int score = 0;
+        int obtacleSpeed = 10;
+        Random rand = new Random();
+        int position;
+        bool isGameOver = false;
+
+
+
         public Form1()
         {
             InitializeComponent();
+
+            GameReset();
+        }
+
+
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            trex.Top += jumpSpeed;
+
+            textScore.Text = "Score: " + score;
+
+            if (jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            if (trex.Top > 360 && jumping == false)
+            {
+                force = 12;
+                trex.Top = 361;
+                jumpSpeed = 0;
+            }
+
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= obtacleSpeed;
+
+                    if (x.Left < -100)
+                    {
+                        x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                        score++; 
+                    }
+
+                    if (trex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+                        trex.Image = Properties.Resources.dead;
+                        textScore.Text += " Press R to restart the game!";
+                        isGameOver = true;
+                    }
+                }
+            }
+
+            if (score > 5 && score < 10)
+            {
+                obtacleSpeed = 12; 
+            } else if (score >= 10) {
+                obtacleSpeed = 15; 
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && jumping == false)
+            {
+                jumping = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+
+            if (e.KeyCode == Keys.R && isGameOver == true)
+            {
+                GameReset();
+            }
+        }
+
+        private void GameReset()
+        {
+            force = 12;
+            jumpSpeed = 0;
+            jumping = false;
+            score = 0;
+            obtacleSpeed = 10;
+            textScore.Text = "Score: " + score;
+            trex.Image = Properties.Resources.running;
+            isGameOver = false;
+            trex.Top = 361;
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    position = this.ClientSize.Width + rand.Next(500, 800) + (x.Width * 10);
+
+                    x.Left = position;
+                }
+            }
+
+            gameTimer.Start();
         }
     }
 }
